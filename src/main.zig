37d1896b2cbf9fabf8d98d6b36be94be9d9e71fd
Jefferson Hernandez <jeffersonh44@gmail.com>
2024-03-25 20:56:12 -0500

    working version for max values of all numbers in file

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,48 @@
 const std = @import("std");
+const stdout = std.io.getStdOut().writer();
+const fs = std.fs;
+const print = std.debug.print;
+
+fn printStation(station: []const u8) !void {
+    try stdout.print("hello station: {s}.\n", .{station});
+}
 
 pub fn main() !void {
-    const stdout = std.io.getStdOut().writer();
-    try stdout.print("Hello {s}!\n", .{"world"});
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    const file = try fs.cwd().openFile("./measurements.txt", .{});
+    defer file.close();
+
+    // Wrap the file reader in a buffered reader.
+    // Since it's usually faster to read a bunch of bytes at once.
+    var buf_reader = std.io.bufferedReader(file.reader());
+    const reader = buf_reader.reader();
+
+    var line = std.ArrayList(u8).init(allocator);
+    defer line.deinit();
+
+    const writer = line.writer();
+    var line_no: usize = 1;
+    var max_value: f16 = 0.0;
+    while (reader.streamUntilDelimiter(writer, '\n', null)) : (line_no += 1) {
+        // Clear the line so we can reuse it.
+        defer line.clearRetainingCapacity();
+
+        var splitted_line = std.mem.split(u8, line.items, ";");
+        _ = splitted_line.next();
+        var value = splitted_line.next();
+        max_value = @max(max_value, try std.fmt.parseFloat(f16, value.?));
+
+        // print("{?s} value: {?s}\n", .{ name, value });
+        //if (line_no == 5) {
+        //    break;
+        //}
+    } else |err| switch (err) {
+        error.EndOfStream => {}, // Continue on
+        else => return err, // Propagate error
+    }
+
+    print("max value: {d}", .{max_value});
 }
