0aa2577b33e48e5e6362ef73c203c77a85c3b6ec
Jefferson Hernandez <jeffersonh44@gmail.com>
2024-04-20 14:40:02 -0500

    working version for the max values per station using hashmaps

--- src/main.zig
+++ src/main.zig
@@ -25,24 +25,45 @@ pub fn main() !void {
 
     const writer = line.writer();
     var line_no: usize = 1;
-    var max_value: f16 = 0.0;
+    // var max_value: f16 = 0.0;
+    // var stationsMaxMap = std.StringArrayHashMapUnmanaged(f16){};
+    var stationsMaxMap = std.StringHashMap(f16).init(std.heap.page_allocator);
+    defer stationsMaxMap.deinit();
     while (reader.streamUntilDelimiter(writer, '\n', null)) : (line_no += 1) {
         // Clear the line so we can reuse it.
         defer line.clearRetainingCapacity();
 
-        var splitted_line = std.mem.split(u8, line.items, ";");
-        _ = splitted_line.next();
-        var value = splitted_line.next();
-        max_value = @max(max_value, try std.fmt.parseFloat(f16, value.?));
+        var splitted_line = std.mem.tokenize(u8, line.items, ";");
+        const station = splitted_line.next().?;
+        const value = try std.fmt.parseFloat(f16, splitted_line.rest());
+
+        if (stationsMaxMap.get(station)) |currentValue| {
+            try stationsMaxMap.put(station, @max(currentValue, value));
+        } else {
+            // std.debug.print("hello here {s}\n", .{station});
+            const clonedStation = try std.mem.Allocator.dupe(std.heap.page_allocator, u8, station);
+            try stationsMaxMap.put(clonedStation, value);
+        }
+
+
+
+        // max_value = @max(max_value, try std.fmt.parseFloat(f16, value.?));
 
         // print("{?s} value: {?s}\n", .{ name, value });
-        //if (line_no == 5) {
+        // if (line_no == 5) {
         //    break;
-        //}
+        // }
     } else |err| switch (err) {
         error.EndOfStream => {}, // Continue on
         else => return err, // Propagate error
     }
 
-    print("max value: {d}", .{max_value});
+    var iter = stationsMaxMap.iterator();
+    while (iter.next()) |entry| {
+        const key = entry.key_ptr.*;
+        const value = entry.value_ptr.*;
+
+        print("Station: {s}, max value: {d}\n",.{key, value});
+    }
+    // print("max value: {d}", .{max_value});
 }
